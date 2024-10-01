e291329f0be5b64b387452556014c0e8a4cf9418
Agustin <agusrod9@gmail.com>
2024-09-30 21:54:31 -0300

    fix en shuffle Fisher-Yates sorting algorithm

--- script.js
+++ script.js
@@ -38,18 +38,11 @@ winnerOverlay.addEventListener("click", ()=>{
 })
 
 const shuffle =(array)=>{
-    let currentIndex = array.length;
-
-    // While there remain elements to shuffle...
-    while (currentIndex != 0) {
-        // Pick a remaining element...
-        let randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
-
-    // And swap it with the current element.
-        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
-    }
-    return array;
+    for (let i = array.length - 1; i > 0; i--) { 
+        const j = Math.floor(Math.random() * (i + 1)); 
+        [array[i], array[j]] = [array[j], array[i]]; 
+      } 
+      return array; 
 }
 
 const getAllPokemons = async()=>{
